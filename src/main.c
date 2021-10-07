9c3d761453bd4c950fa6dd1021803a0cbdf36f91
mfcoronaortega <mfcoronaortega@miners.utep.edu>
2021-10-07 17:05:18 -0600

    Added history functionality to main

--- src/main.c
+++ src/main.c
@@ -2,14 +2,17 @@
 #include <string.h>
 #include <stdlib.h>
 #include "tokenizer.c"
+#include "history.c"
 
 #define MAX 50
 
 void main(void){
   //Simple UI
   char userInput[MAX];
+  List* history = init_history();
+  char** tokens;
   
-  printf("Welcome to my Tokenizer! \n Enter a phrase after '$' or enter 'e' to exit.\n");
+  printf("Welcome to my Tokenizer! \n Enter a phrase after '$'.\n Enter 'e' to exit or '!' to see your phrases so far\n");
 
   while(*userInput != 'e')
     {
@@ -21,13 +24,21 @@ void main(void){
 	{
 	  printf("The string you entered: %s", &userInput); //Test Echo
       
-	  char **tokens = tokenize(userInput);
+	  tokens = tokenize(userInput);
 
 	  printf("The sting you entered Tokenized is:\n");
 
 	  print_tokens(tokens);
-	  free_tokens(tokens);
+	  
+	  add_history(history, userInput);
 	}
+      if(*userInput == '!'){
+	printf("Your Phrases so far:\n");
+	print_history(history);
+      }
   }
+  free_tokens(tokens);
+  free_history(history);
+  //print_history(history);//checks if list is empty
   printf("Thank you for using my Tokenizer! GOODBYE!\n");
 }
